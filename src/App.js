d46166bb7a1a8368db12a5ea48d26d45601221ed
Wagner Reis <wagnerreis1997@gmail.com>
2020-01-08 14:57:30 -0300

    Usando o ContextAPI para Autenticação

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 import NewComment from './NewComment'
 import Comments from './Comments'
+import CreateUser from './CreateUser'
 
-/*firebase 
-  .auth()
-  .createUserWithEmailAndPassword('wagnerreis@gmail.com', 'abc123')
-  .then( user => {
-    //user.displayName= 'Wagner Reis'
-    //firebase.auth().updateCurrentUser(user)
-  })*/
-
-  // firebase.auth().onAuthStateChanged(user => {
-  //   if(user) {
-  //     console.log(user.displayName)
-  //     user.updateProfile({ displayName: 'Jorge Silva' })
-  //   }
-  // })
+import { AuthProvider } from './auth'
 
 function App() {
   return (
-    <div>
-      <NewComment />
-      <Comments />
-    </div>
+    <AuthProvider>
+      <div>
+        <NewComment />
+        <Comments />
+        <CreateUser />
+      </div>
+    </AuthProvider>
   )
 }
 
